30248905984db01af72f25e25bc20d2206e4964e
vinayak-chavan <72243768+vinayak-chavan@users.noreply.github.com>
2023-04-24 23:58:07 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MongoURI: 'mongodb://localhost:27017/Online_shopping'
-    // MongoURI: 'mongodb+srv://chavanvinayak017:chavanvinayak017@cluster0.venjqbu.mongodb.net/test'
+    // MongoURI: 'mongodb://localhost:27017/Online_shopping'
+    MongoURI: 'mongodb+srv://chavanvinayak017:chavanvinayak017@cluster0.venjqbu.mongodb.net/test'
 }
